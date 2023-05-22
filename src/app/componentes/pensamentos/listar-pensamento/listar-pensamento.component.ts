1f82d058dba1850e6e4aaa090a89d0aa2ef68f56
Daniela Leguari <danielaleguari@outlook.com>
2023-05-22 17:44:27 -0300

    ajuste no titulo

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -16,6 +16,7 @@ export class ListarPensamentoComponent {
   filtro: string = '';
   favoritos: boolean = false;
   listaFavoritos: Pensamento[] = [];
+  titulo: string = 'Meu Mural'
 
   constructor(
     private service: PensamentoService,
@@ -48,6 +49,7 @@ export class ListarPensamentoComponent {
   }
 
   listarFavoritos() {
+    this.titulo = 'Meus Favoritos'
     this.favoritos = true;
     this.haMaisPensamentos = true;
     this.paginaAtual = 1;
